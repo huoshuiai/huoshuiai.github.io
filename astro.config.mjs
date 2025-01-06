4bb7b265d23a445f0738b641c96952801e15c808
elexingyu <chenxychn@outlook.com>
2025-01-06 11:42:43 +0800

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
-  output: 'static',
-  build: {
-    inlineStylesheets: 'auto'
-  },
-  vite: {
-    build: {
-      assetsInlineLimit: 0
-    }
-  }
+  site: 'https://huoshuiai.github.io',
+  base: '/huangruolan.github.io',
 });
